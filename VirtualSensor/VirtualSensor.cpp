9df0da14718eed7c51852963f8a9357a723026ae
Behnam Shakibafar <behnam.shakibafar@gmail.com>
2024-07-06 21:05:06 -0400

    implement the Command Interface
    command interface implemented as explained in the document

--- VirtualSensor/VirtualSensor.cpp
+++ VirtualSensor/VirtualSensor.cpp
@@ -2,22 +2,69 @@
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <map>
 #include "SensorTypes.h"
 
+using namespace std;
 
-    int main()
+// Command Processor Class
+class CommandProcessor {
+private:
+
+public:
+    void processCommand() 
     {
-        std::cout << "Hi there!" << std::endl;
-        int i = 0;
+        string command;
+        cout << "Enter command (add <sensortype>, list, remove <sensorid>): ";
+        getline(cin, command);
 
-        Sensor* sensor_array[16];
-
-        for (i = 0; i < 16; i ++)
+        // Parse command
+        string action;
+        string arg;
+        size_t pos = command.find(' ');
+        if (pos != string::npos) 
         {
-            sensor_array[i] = new PressureSensor(i);
-            sensor_array[i]->displaySensorInfo();
+            action = command.substr(0, pos);
+            arg = command.substr(pos + 1);
+        }
+        else 
+        {
+            action = command;
         }
 
-        std::cout << "Bye!" << std::endl;
-        return 0;
+        // Process command
+        if (action == "add") 
+        {
+
+        }
+        else if (action == "list") 
+        {
+
+        }
+        else if (action == "remove") 
+        {
+
+        }
+        else
+        {
+            cout << "Error: Unknown command. Supported commands: add, list, remove." << endl;
+        }
     }
+};
+
+
+// Main function
+int main() 
+{
+    CommandProcessor processor;
+
+    while (1)
+    {
+        processor.processCommand();
+        cout << endl;
+    }
+
+    return 0;
+}
